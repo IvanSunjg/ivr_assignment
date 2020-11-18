3a0c89da809571a5215ad87e88dd287a87b33e2f
IvanSunjg <58193625+IvanSunjg@users.noreply.github.com>
2020-11-18 22:13:00 +0800

    Update state_estimation.py

--- ivr_assignment/src/state_estimation.py
+++ ivr_assignment/src/state_estimation.py
@@ -5,7 +5,8 @@ import sys
 import rospy
 import cv2
 import numpy as np
-from std_msgs.msg import Float64MultiArray, Int16MultiArray
+import message_filters
+from std_msgs.msg import Float64MultiArray, Int16MultiArray, Float64
 
 class joint_state:
     def __init__(self):
@@ -13,12 +14,19 @@ class joint_state:
         rospy.init_node('joint_processing', anonymous=True)
 
         # initialize subscribers to receive the coordinates of the joints
-        self.robot_x_z = rospy.Subscriber("/robot/x_z", Int16MultiArray, self.callback1)
-        self.robot_y_z = rospy.Subscriber("/robot/y_z", Int16MultiArray, self.callback2)
+        self.robot_x_z = message_filters.Subscriber("/robot/x_z", Int16MultiArray)
+        self.robot_y_z = message_filters.Subscriber("/robot/y_z", Int16MultiArray)
+        self.sync = message_filters.ApproximateTimeSynchronizer([self.robot_x_z, self.robot_y_z],
+                                                                 10, 0.1,allow_headerless=True)
+
+        self.sync.registerCallback(self.callback)
         # initialize a publisher for joint angles
         self.joints_pub = rospy.Publisher("/joints_ang", Float64MultiArray, queue_size=10)
         # initialize a publisher for target object position
         self.target_pub = rospy.Publisher("/target_pos", Int16MultiArray, queue_size=10)
+        # initialize a publisher for end effector position
+        self.end_eff_pub = rospy.Publisher("/end_pos", Int16MultiArray, queue_size=10)
+
         #[red_x,red_z,green_x,green_z,blue_x,blue_z,target_x,target_z]
         self.x_z = None
         # [red_y,red_z,green_y,green_z,blue_y,blue_z,target_y,target_z]
@@ -41,56 +49,50 @@ class joint_state:
             joint_4 = -joint_4
         return np.array([joint_2,joint_3,joint_4])
 
-    # Recieve data, process it
-    def callback1(self,data):
-        self.x_z = data.data
-        self.joint_estimate()
-    # Recieve data, process it
-    def callback2(self, data):
-        self.y_z = data.data
-        self.joint_estimate()
-
-
-    def joint_estimate(self):
-        if(self.x_z != None and self.y_z != None):
-            # xz [red_x,red_z,green_x,green_z,blue_x,blue_z,target_x,target_z]
-            # yz [red_y,red_z,green_y,green_z,blue_y,blue_z,target_y,target_z]
-            self.red_x = self.x_z[0]
-            self.red_y = self.y_z[0]
-            self.red_z_1 = self.x_z[1]
-            self.red_z_2 = self.y_z[1]
-            self.green_x = self.x_z[2]
-            self.green_y = self.y_z[2]
-            self.green_z_1 = self.x_z[3]
-            self.green_z_2 = self.y_z[3]
-            self.blue_x = self.x_z[4]
-            self.blue_y = self.y_z[4]
-            self.blue_z_1 = self.x_z[5]
-            self.blue_z_2 = self.y_z[5]
-            self.target_x = self.x_z[6]
-            self.target_y = self.y_z[6]
-            self.target_z_1 = self.x_z[7]
-            self.target_z_2 = self.y_z[7]
-            if(self.red_z_2 == 0):self.red_z = self.red_z_1
-            else: self.red_z = self.red_z_2
-            if (self.green_z_2 == 0):self.green_z = self.green_z_1
-            else:self.green_z = self.green_z_2
-            if (self.blue_z_2 == 0):self.blue_z = self.blue_z_1
-            else:self.blue_z = self.blue_z_2
-            if (self.target_z_2 == 0): self.target_z =self.target_z_1
-            else:self.target_z = self.target_z_2
-            self.red = np.array([self.red_x-392,self.red_y-392,self.red_z-529])
-            self.green = np.array([self.green_x-392,self.green_y-392,self.green_z-529])
-            self.blue = np.array([self.blue_x-392,self.blue_y-392,self.blue_z-529])
-            self.target = np.array([392-self.target_x,392-self.target_y,529-self.target_z])
-            #self.yellow = np.array([0,0,0])
-            self.joint_angles = Float64MultiArray()
-            self.joint_angles.data = self.detect_joint_angles(self.red,self.green,self.blue)
-            self.target_pos = Int16MultiArray()
-            self.target_pos.data = self.target
-            # Publish the joint angles
-            self.joints_pub.publish(self.joint_angles)
-            self.target_pub.publish(self.target_pos)
+    def callback(self,x_z_pos,y_z_pos):
+        # xz [red_x,red_z,green_x,green_z,blue_x,blue_z,target_x,target_z]
+        # yz [red_y,red_z,green_y,green_z,blue_y,blue_z,target_y,target_z]
+        self.x_z = x_z_pos.data
+        self.y_z = y_z_pos.data
+        self.red_x = self.x_z[0]
+        self.red_y = self.y_z[0]
+        self.red_z_1 = self.x_z[1]
+        self.red_z_2 = self.y_z[1]
+        self.green_x = self.x_z[2]
+        self.green_y = self.y_z[2]
+        self.green_z_1 = self.x_z[3]
+        self.green_z_2 = self.y_z[3]
+        self.blue_x = self.x_z[4]
+        self.blue_y = self.y_z[4]
+        self.blue_z_1 = self.x_z[5]
+        self.blue_z_2 = self.y_z[5]
+        self.target_x = self.x_z[6]
+        self.target_y = self.y_z[6]
+        self.target_z_1 = self.x_z[7]
+        self.target_z_2 = self.y_z[7]
+        if(self.red_z_2 == 0):self.red_z = self.red_z_1
+        else: self.red_z = self.red_z_2
+        if (self.green_z_2 == 0):self.green_z = self.green_z_1
+        else:self.green_z = self.green_z_2
+        if (self.blue_z_2 == 0):self.blue_z = self.blue_z_1
+        else:self.blue_z = self.blue_z_2
+        if (self.target_z_2 == 0): self.target_z =self.target_z_1
+        else:self.target_z = self.target_z_2
+        self.red = np.array([self.red_x-392,self.red_y-392,self.red_z-529])
+        self.green = np.array([self.green_x-392,self.green_y-392,self.green_z-529])
+        self.blue = np.array([self.blue_x-392,self.blue_y-392,self.blue_z-529])
+        self.target = np.array([392-self.target_x,392-self.target_y,529-self.target_z])
+        self.joint_angles = Float64MultiArray()
+        self.joint_angles.data = self.detect_joint_angles(self.red,self.green,self.blue)
+        self.target_pos = Int16MultiArray()
+        self.target_pos.data = self.target
+        self.end_eff_pos = Int16MultiArray()
+        self.red = np.array([392-self.red_x ,392-self.red_y,529-self.red_z])
+        self.end_eff_pos.data = self.red
+        # Publish the joint angles
+        self.joints_pub.publish(self.joint_angles)
+        self.target_pub.publish(self.target_pos)
+        self.end_eff_pub.publish(self.end_eff_pos)
 
 
 # call the class
